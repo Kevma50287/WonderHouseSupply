1d423b7c6b027689f6d1a897f468e53e33440c7f
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-08-22 15:56:11 -0400

    header start

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,33 @@
 import React from 'react'
-
+import GroupLogo from '../ProjectImajes/5 links center/GroupLogo.png';
+import searchIcon from '../ProjectImajes/5 links center/searchIcon.png';
+import Cart from '../ProjectImajes/5 links center/Cart.png';
 export default function Header() {
   return (
-    <div>Header</div>
+
+    <div className='header'>
+        <div className='header-routes'>
+        
+          <img className="header-route" src={GroupLogo}/>
+    
+          <h3 className="header-route">ABOUT</h3>
+          
+          <h3 className="header-route">PRODUCTS</h3>
+          
+          <h3 className="header-route">SERVICES</h3>
+
+          <button>
+          <img src={searchIcon}/>
+          </button>
+
+          <button>
+          <img src={Cart}/>
+          </button>
+          
+          
+      </div>
+    </div>
+    
+
   )
 }
