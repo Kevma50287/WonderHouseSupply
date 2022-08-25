c73a6cb7aa965cfb1633e150190474b72e8d0d9c
Kevin <magavakevin10@gmail.com>
2022-08-24 20:28:11 -0400

    Merged and updated

--- src/App.js
+++ src/App.js
@@ -27,147 +27,104 @@ function App() {
   // data of our user
   const [userData, setUserData] = useState({ Cart: [] })
 
-  function cartChangeNumberOfItemsCallBackFunction(qtyCart,id,setQtyCart){
+  function cartChangeNumberOfItemsCallBackFunction(qtyCart, id, setQtyCart) {
     let qtyy = qtyCart
     setQtyCart(qtyy)
-    
-
 
     let newUserCart = userData.Cart.map((item) => {
       if (item.id === `${id}`) { return { id: item.id, numberOfItemsToBuy: `${qtyy}` } }
       else { return item }
     })
 
-    
-
-    let newUserDataUpdate = {...userData}
+    let newUserDataUpdate = { ...userData }
     newUserDataUpdate.Cart = newUserCart
 
-    console.log(newUserDataUpdate)
-    // const updateObj = {Cart : newUserCart}
     setUserData(newUserDataUpdate)
 
-      
-    // delete newUserDataUpdate.id;
     console.log(userData)
-    // console.log(patchUserData)
-     if (logedIn === `${true}`)
-     {
-      
-  
-      fetch(`http://localhost:3002/users/${userData.id}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newUserDataUpdate)
-     })
-     .then ((response) => response.json())
-     .then(data => data)}
-     else {}
 
-     setQtyCart("1")
+    if (logedIn === `${true}`) {
+      fetch(`http://localhost:3002/users/${userData.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newUserDataUpdate)
+      })
+        .then((response) => response.json())
+        .then(data => data)
+    }
+    else { }
+
+    setQtyCart("1")
 
   }
 
   //function to delete
   function cartDeleteCallBackFunction(id) {
-
-   
     let newUserCart = userData.Cart.filter((item) => item.id !== `${id}`)
 
-    let newUserDataDelete = {...userData}
+    let newUserDataDelete = { ...userData }
     newUserDataDelete.Cart = newUserCart
 
     console.log(newUserDataDelete.Cart)
-    // console.log(newUserCart)
-    // const updateObj = {Cart : newUserCart}
     setUserData(newUserDataDelete)
 
-    
-    // delete newUserDataDelete.id;
-  // console.log(patchUserData)
-   if (logedIn === `${true}`)
-   {
-    
+    if (logedIn === `${true}`) {
+      fetch(`http://localhost:3002/users/${userData.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newUserDataDelete)
+      })
+        .then((response) => response.json())
+        .then(data => data)
+    }
+    else { }
 
-    fetch(`http://localhost:3002/users/${userData.id}`, {
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(newUserDataDelete)
-   })
-   .then ((response) => response.json())
-   .then(data => data)}
-   else {}
-    
   }
-
-  // console.log(userData)
-
-
 
   //function to add something to your cart
   function cartUpdateCallBackFunction(qty, id, setQty) {
     setQty("1")
-    // console.log(userData)
-
     let newUserCart = userData.Cart.map((item) => {
       if (item.id === `${id}`) { return { id: item.id, numberOfItemsToBuy: `${+item.numberOfItemsToBuy + +qty}` } }
       else { return item }
     })
-
-
     // if (newUserCart === userData.Cart) {
     let newUserCartTwo = newUserCart.filter((item) => {
-      if (item.id == `${id}`) { return item } else { }
+      if (item.id === `${id}`) { return item } else { }
     })
-  }
+
 
 
     if (newUserCartTwo.length === 0) {
       newUserCart = [...userData.Cart, { id: `${id}`, numberOfItemsToBuy: qty }]
-    } else {  }
-
-
+    } else { }
 
 
     setUserData((userData) => {
       Object.keys(userData).map((key) => {
         if (key === "Cart") {
           userData[key] = newUserCart
-        } else { userData[key] = userData[key] }
+        } else { 
+          userData[key] = userData[key] 
+        }
       })
-      // console.log(userData)
       return (userData)
-      
     }
     )
 
-    let patchUserData = {...userData}
-      delete patchUserData.id;
-    // console.log(patchUserData)
-     if (logedIn === `${true}`)
-     {
-      
+    let patchUserData = { ...userData }
+    delete patchUserData.id;
 
+    if (logedIn === `${true}`) {
       fetch(`http://localhost:3002/users/${userData.id}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(patchUserData)
-     })
-     .then ((response) => response.json())
-     .then(data => data)}
-     else {}
-    
-   
-    // 
-    // } else {}
-    // console.log(userData)
-    //  console.log(newUserCart)
-    // console.log(logedIn)
-    // console.log(qty)
-    // console.log(id)
-
-
-
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(patchUserData)
+      })
+        .then((response) => response.json())
+        .then(data => data)
+    }
+    else { }
   }
 
 
@@ -187,16 +144,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Main itemData={itemData} />} />
         <Route path="/about" element={<AboutRoute />} />
-        <Route path="/cart" element={<Cart userData={userData} itemData={itemData} cartDeleteCallBackFunction={cartDeleteCallBackFunction}  cartChangeNumberOfItemsCallBackFunction={cartChangeNumberOfItemsCallBackFunction}/>} />
+        <Route path="/cart" element={<Cart userData={userData} itemData={itemData} cartDeleteCallBackFunction={cartDeleteCallBackFunction} cartChangeNumberOfItemsCallBackFunction={cartChangeNumberOfItemsCallBackFunction} />} />
         <Route path="/logIn" element={<LogIn setUserData={setUserData} setLogedIn={setLogedIn} />} />
         <Route path="/products" element={
           <ProductsRoute
             itemData={itemData}
-            cartUpdateCallBackFunction={cartUpdateCallBackFunction}
           />
         }>
           <Route index element={<ItemPage />} />
-          <Route path=":productId" element={<ProductInfo />} />
+          <Route path=":productId" element={<ProductInfo cartUpdateCallBackFunction={cartUpdateCallBackFunction} />} />
         </Route>
         <Route path="/services" element={<ServicesRoute />} />
         <Route path="/signUp" element={<SignUp />} />
@@ -207,6 +163,7 @@ function App() {
     </div>
   );
 }
+
 
 export { itemURL, userURL, reviewsURL }
 export default App;
