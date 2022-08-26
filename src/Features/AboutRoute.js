0754981ce68b4a4bf3f5e527eec9b0b4a463f024
Kevin <magavakevin10@gmail.com>
2022-08-26 13:05:13 -0400

    about

--- src/Features/AboutRoute.js
+++ src/Features/AboutRoute.js
@@ -1,7 +1,22 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+
 
 export default function AboutRoute() {
     return (
-        <h1 className='main'>AboutRoute!</h1>
+        <>
+            <h1 className='main'>About</h1>
+            <Grid container>
+                <Grid item sm={6}>
+                    <h1>Special thanks to Tsering for helping</h1>
+                </Grid>
+                <Grid item sm={6}>
+                    <div>
+                        <img src = "../../../CrazyProgram/images/store.jpg" alt =""/>
+                    </div>
+                </Grid>
+            </Grid>
+        </>
+
     )
 }
--- src/Features/ConfirmationPage.js
+++ src/Features/ConfirmationPage.js
@@ -4,7 +4,12 @@ const ConfirmationPage = () => {
   return (
     <div>
         <h1>Checkout Confirmed!</h1>
-        <h5>Please pickup and pay in person at the store. Thank you for choosing Wonder House Supply and we hope to see you soon!</h5>
+        <h3>Please pickup and pay in person at the store. Thank you for choosing Wonder House Supply and we hope to see you soon!</h3>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
     </div>
   )
 }
