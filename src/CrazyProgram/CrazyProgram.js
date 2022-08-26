405ccd26343b0c0b841399b76b7ac2b41b36ec7c
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-08-26 10:02:31 -0400

    try to do css

---
+++ src/CrazyProgram/CrazyProgram.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import defaultlogo from './images/defaultlogo.png';
+import Frame from './images/Frame.png';
+import Frame1 from './images/Frame1.png';
+import Frame2 from './images/Frame2.png';
+import g48032 from './images/g48032.png';
+import image33 from './images/image33.png';
+import image36 from './images/image36.png';
+import image39 from './images/image39.png';
+import Page1 from './images/Page1.png';
+import Vector from './images/Vector.png';
+
+
+const CrazyProgram = () => {
+  return (
+    <div className="kevinHelps">
+        <section className="u-align-center u-clearfix u-white u-section-1" id="sec-77fa">
+            <img className="u-image u-image-default u-image-1" src={image39} alt="" data-image-width="456" data-image-height="304" data-animation-name="" data-animation-duration="0" data-animation-direction=""></img>
+            <img className="u-image u-image-round u-radius-10 u-image-2" src={image36} alt="" data-image-width="424" data-image-height="635"></img>
+            <h1 className="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-text u-text-custom-color-1 u-text-default u-text-1"> Find the Perfect Solution for your Home Repairs</h1>
+            <p className="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-text u-text-custom-color-2 u-text-2"> At Wonder House Supply Inc. we offer experienced advice and recommendations for any common home repairs that you may have.</p>
+            <a href="#" className="u-btn u-btn-rectangle u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-color-3 u-radius-0 u-btn-1"> Explore Products</a>
+            <img className="u-image u-image-contain u-image-default u-image-3" src={Frame} alt="" data-image-width="257" data-image-height="58"></img>
+            <img className="u-image u-image-contain u-image-default u-preserve-proportions u-image-4" src={Vector} alt="" data-image-width="92" data-image-height="92"></img>
+            <img className="u-image u-image-default u-preserve-proportions u-image-5" src={image33} alt="" data-image-width="112" data-image-height="102"></img>
+            <img className="u-image u-image-default u-preserve-proportions u-image-6" src={g48032} alt="" data-image-width="198" data-image-height="31"></img>
+            <img className="u-image u-image-contain u-image-default u-preserve-proportions u-image-7" src={Frame1} alt="" data-image-width="201" data-image-height="49"></img>
+            <img className="u-image u-image-default u-preserve-proportions u-image-8" src={Page1} alt="" data-image-width="100" data-image-height="54"></img>
+            <img className="u-image u-image-default u-preserve-proportions u-image-9" src={Frame2} alt="" data-image-width="200" data-image-height="44"></img>
+        </section>
+    </div>
+  )
+}
+
+export default CrazyProgram
+
+
+
+// <section className="u-align-center u-clearfix u-white u-section-1" id="sec-77fa">
+// <img className="u-image u-image-default u-image-1" src="images/image39.png" alt="" data-image-width="456" data-image-height="304" data-animation-name="" data-animation-duration="0" data-animation-direction="">
+// <img className="u-image u-image-round u-radius-10 u-image-2" src="images/image36.png" alt="" data-image-width="424" data-image-height="635">
+// <h1 className="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-text u-text-custom-color-1 u-text-default u-text-1"> Find the Perfect Solution for yo​ur&nbsp;<br>Ho​me Repairs
+// </h1>
+// <p className="u-align-center-lg u-align-center-md u-align-center-sm u-align-center-xs u-text u-text-custom-color-2 u-text-2"> At Wonder House Supply Inc. we offer experienced advice and recommendations for any common home repairs that you may have.</p>
+// <a href="#" className="u-btn u-btn-rectangle u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-color-3 u-radius-0 u-btn-1"> Explore Products</a>
+// <img className="u-image u-image-contain u-image-default u-image-3" src="images/Frame.png" alt="" data-image-width="257" data-image-height="58">
+// <img className="u-image u-image-contain u-image-default u-preserve-proportions u-image-4" src="images/Vector.png" alt="" data-image-width="92" data-image-height="92">
+// <img className="u-image u-image-default u-preserve-proportions u-image-5" src="images/image33.png" alt="" data-image-width="112" data-image-height="102">
+// <img className="u-image u-image-default u-preserve-proportions u-image-6" src="images/g48032.png" alt="" data-image-width="198" data-image-height="31">
+// <img className="u-image u-image-contain u-image-default u-preserve-proportions u-image-7" src="images/Frame1.png" alt="" data-image-width="201" data-image-height="49">
+// <img className="u-image u-image-default u-preserve-proportions u-image-8" src="images/Page1.png" alt="" data-image-width="100" data-image-height="54">
+// <img className="u-image u-image-default u-preserve-proportions u-image-9" src="images/Frame2.png" alt="" data-image-width="200" data-image-height="44">
+// </section>
