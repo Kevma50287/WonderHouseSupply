33f5c4d186c436a069386e18732051b404bcc1a2
Kevin <magavakevin10@gmail.com>
2022-08-26 12:47:17 -0400

    Updated Cart and Confirmation

---
+++ src/Features/ConfirmationPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ConfirmationPage = () => {
+  return (
+    <div>
+        <h1>Checkout Confirmed!</h1>
+        <h5>Please pickup and pay in person at the store. Thank you for choosing Wonder House Supply and we hope to see you soon!</h5>
+    </div>
+  )
+}
+
+export default ConfirmationPage
